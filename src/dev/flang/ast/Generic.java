257d7950708e54c01ea07cbf1d29e544d92cd6dd
fridis <fridi@fridi.de>
2022-04-10 16:24:09 +0200

    ast: Remove Generic._select/_selectFrom and code using it

--- src/dev/flang/ast/Generic.java
+++ src/dev/flang/ast/Generic.java
@@ -66,25 +66,6 @@ public class Generic extends ANY
 
 
   /**
-   * In case this is a generic that selects a particular argument of an open
-   * generic, this gives the index of that argument in the actual generics for
-   * the open generic.
-   *
-   * Otherwise, this is -1.
-   */
-  private final int _select;
-
-
-  /**
-   * In case this is a generic that selects a particular argument of an open
-   * generic, this gives the original open generic.
-   *
-   * Otherwise, this is null.
-   */
-  private final Generic _selectFrom;
-
-
-  /**
    * The type parameter this generic corresponds to
    */
   private AbstractFeature _typeParameter;
@@ -104,32 +85,7 @@ public class Generic extends ANY
   public Generic(AbstractFeature typeParameter, int index)
   {
     _index = index;
-    _select = -1;
-    _selectFrom = null;
     _typeParameter = typeParameter;
-  }
-
-
-  /**
-   * Constructor used by select() to create a Generic that selects on particular
-   * actual generic passed to an open generic argument.
-   *
-   * @param original the original open generic argument
-   *
-   * @param select the index of the actual argument that is selected, 0 for the
-   * first actual argument.
-   */
-  private Generic(Generic original, int select)
-  {
-    if (PRECONDITIONS) require
-      (original.isOpen(),
-       select >= 0);
-
-    _index = original._index + select;
-    _formalGenerics = original._formalGenerics;
-    _select = select;
-    _selectFrom = original;
-    _typeParameter = _typeParameter;
   }
 
 
@@ -221,8 +177,7 @@ public class Generic extends ANY
    */
   public String name()
   {
-    var n = typeParameter().featureName().baseName();
-    return _select < 0 ? n : n + "." + _select;
+    return typeParameter().featureName().baseName();
   }
 
 
@@ -248,34 +203,16 @@ public class Generic extends ANY
       (!isOpen(),
        Errors.count() > 0 || _formalGenerics.sizeMatches(actualGenerics));
 
-    AbstractType result;
-    if (_select >= 0)
+    int i = index();
+    var actuals = actualGenerics.iterator();
+    while (i > 0 && actuals.hasNext())
       {
-        var openTypes = _selectFrom.replaceOpen(actualGenerics);
-        result = _select < openTypes.size()
-          ? openTypes.get(_select)
-          : // This is not an error, we can run into this situation, e.g., for
-            // the values of a tuple for all the actual clazzes of tuple with
-            // fewer actual generic arguments than the max size of a tuple. In
-            // this case, the types of all the unused select fields will be
-            // t_unit.
-            Types.resolved.t_unit;
+        actuals.next();
+        i--;
       }
-    else
-      {
-        result = null;
-        int i = index();
-        var actuals = actualGenerics.iterator();
-        while (i > 0 && actuals.hasNext())
-          {
-            actuals.next();
-            i--;
-          }
-        if (CHECKS) check
-          (Errors.count() > 0 || actuals.hasNext());
-        result = actuals.hasNext() ? actuals.next() : Types.t_ERROR;
-      }
-    return result;
+    if (CHECKS) check
+      (Errors.count() > 0 || actuals.hasNext());
+    return actuals.hasNext() ? actuals.next() : Types.t_ERROR;
   }
 
 
@@ -316,25 +253,6 @@ public class Generic extends ANY
     // Now, return the tail of actuals:
     return actuals.hasNext() ? new List<>(actuals)
                              : Type.NONE;
-  }
-
-
-  /**
-   * For an open generic, create a Type that selects one given actual generic
-   * argument.
-   *
-   * @param i the index of the actual generic argument, must be >= 0.
-   *
-   * @return a Type that represents the given actual generic argument or t_unit
-   * if the index is >= the number of actual generic arguments.
-   */
-  public Type select(int i)
-  {
-    if (PRECONDITIONS) require
-      (isOpen(),
-       i >= 0);
-
-    return new Type(_typeParameter.pos(), new Generic(this, i));
   }
 
 
