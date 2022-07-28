c31b1539b13e3e0b6090d2d935fd12e245b85289
fridis <fridi@fridi.de>
2022-07-28 17:39:53 +0200

    dfa: cosmetics: Fixed vertical alignment of code

--- src/dev/flang/fuir/analysis/dfa/Value.java
+++ src/dev/flang/fuir/analysis/dfa/Value.java
@@ -66,13 +66,13 @@ public class Value extends ANY
         else if (a == UNIT                    || b == UNIT                   ) { return a == UNIT  ? +1 : -1; }
         else if (a instanceof Instance     ai && b instanceof Instance     bi) { return ai.compareTo(bi);     }
         else if (a instanceof NumericValue an && b instanceof NumericValue bn) { return an.compareTo(bn);     }
-        else if (a instanceof RefValue   ab && b instanceof RefValue   bb) { return ab.compareTo(bb);     }
+        else if (a instanceof RefValue     ab && b instanceof RefValue     bb) { return ab.compareTo(bb);     }
         else if (a instanceof TaggedValue  at && b instanceof TaggedValue  bt) { return at.compareTo(bt);     }
         else if (a instanceof SysArray     aa && b instanceof SysArray     ba) { return aa.compareTo(ba);     }
         else if (a instanceof ValueSet     as && b instanceof ValueSet     bs) { return as.compareTo(bs);     }
         else if (a instanceof Instance    ) { return +1; } else if (b instanceof Instance       ) { return -1; }
         else if (a instanceof NumericValue) { return +1; } else if (b instanceof NumericValue   ) { return -1; }
-        else if (a instanceof RefValue  ) { return +1; } else if (b instanceof RefValue     ) { return -1; }
+        else if (a instanceof RefValue    ) { return +1; } else if (b instanceof RefValue       ) { return -1; }
         else if (a instanceof TaggedValue ) { return +1; } else if (b instanceof TaggedValue    ) { return -1; }
         else if (a instanceof SysArray    ) { return +1; } else if (b instanceof SysArray       ) { return -1; }
         else if (a instanceof ValueSet    ) { return +1; } else if (b instanceof ValueSet       ) { return -1; }
