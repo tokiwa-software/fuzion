195abd9ef3f5163c875ef9bdfd6cdbf5428974a3
Michael Lill <michael.lill@tokiwa.software>
2022-08-31 13:07:13 +0200

    parser: fix spelling issue in antlr grammar this led to a warning during build

--- src/dev/flang/parser/Parser.java
+++ src/dev/flang/parser/Parser.java
@@ -3680,7 +3680,7 @@ onetype     : "ref" simpletype
             | simpletype "->" simpletype
             | pTypeList "->" simpletype
             | pTypeList
-            | LPAREN type RPARENT typeTail
+            | LPAREN type RPAREN typeTail
             | simpletype
             ;
 pTypeList   : LPAREN typeList RPAREN
