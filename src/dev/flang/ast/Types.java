e81fadb323f5dcab9cf4cc877a548f38efb48f85
Michael Lill <michael.lill@tokiwa.software>
2022-11-16 16:34:53 +0100

    tests: reg issue 309, switch test to negative

--- src/dev/flang/ast/Types.java
+++ src/dev/flang/ast/Types.java
@@ -28,7 +28,6 @@ package dev.flang.ast;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -52,7 +51,6 @@ public class Types extends ANY
 
 
   private static Map<Type, Type> types;
-
 
   /**
    * Name of abstract features for function types:
