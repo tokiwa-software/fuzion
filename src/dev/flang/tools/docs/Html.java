0316fb1d3d94f4a7fc8735c0ddaaec12bc24569b
Michael Lill <michael.lill@tokiwa.software>
2022-11-17 14:07:34 +0100

    docs: fix legal issues

--- src/dev/flang/tools/docs/Html.java
+++ src/dev/flang/tools/docs/Html.java
@@ -1,6 +1,6 @@
 /*
 
-This file is part of the Fuzion docs generator implementation.
+This file is part of the Fuzion language implementation.
 
 The Fuzion docs generator implementation is free software: you can redistribute it
 and/or modify it under the terms of the GNU General Public License as published
