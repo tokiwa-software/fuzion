2abbe452437331bcd27e7e433d9829f8af9c37e4
Wael Youssfi <youssfi.wael1997@gmail.com>
2022-11-21 16:50:43 +0100

    typo

--- src/dev/flang/fuir/analysis/dfa/DFA.java
+++ src/dev/flang/fuir/analysis/dfa/DFA.java
@@ -924,7 +924,7 @@ public class DFA extends ANY
     put("fuzion.std.fileio.move"         , cl -> cl._dfa._bool );
     put("fuzion.std.fileio.create_dir"   , cl -> cl._dfa._bool );
     put("fuzion.std.fileio.open_source"  , cl -> new NumericValue(cl._dfa, cl._dfa._fuir.clazzResultClazz(cl._cc)) );
-    put("fuzion.std.fileio.open_source"  , cl -> cl._dfa._bool );
+    put("fuzion.std.fileio.close_source" , cl -> cl._dfa._bool );
     put("fuzion.std.out.flush"           , cl -> Value.UNIT );
     put("fuzion.std.err.flush"           , cl -> Value.UNIT );
     put("fuzion.stdin.nextByte"          , cl -> new NumericValue(cl._dfa, cl._dfa._fuir.clazzResultClazz(cl._cc)) );
