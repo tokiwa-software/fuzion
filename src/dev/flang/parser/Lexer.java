b198ef91d7b143bf1a6c3d05b380a6f6128301d1
fridis <fridi@fridi.de>
2022-12-02 16:10:13 +0100

    parser: add 'fixed' moidifier for methods
    This is the opposite of 'dyn', maybe gonna drop 'dyn' later.

--- src/dev/flang/parser/Lexer.java
+++ src/dev/flang/parser/Lexer.java
@@ -161,6 +161,7 @@ public class Lexer extends SourceFile
     t_in("in"),
     t_do("do"),
     t_dyn("dyn"),
+    t_fixed("fixed"),
     t_loop("loop"),
     t_while("while"),
     t_until("until"),
