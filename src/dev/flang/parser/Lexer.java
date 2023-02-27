d0077a27332cf3a2a725e26eadcf19b6d6a44725
fridis <fridi@fridi.de>
2023-02-27 16:20:48 +0100

    parser: Allow `universe.this.x` to explicitly call `x` declared in the universe. fix #1168

--- src/dev/flang/parser/Lexer.java
+++ src/dev/flang/parser/Lexer.java
@@ -191,6 +191,7 @@ public class Lexer extends SourceFile
     t_public("public"),
     t_of("of"),
     t_type("type"),
+    t_universe("universe"),
     t_eof,               // end of file
     t_indentationLimit,  // token's indentation is not sufficient
     t_lineLimit,         // token is in next line while sameLine() parsing is enabled
