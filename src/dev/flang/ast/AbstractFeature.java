4a19575aa974ab1401904079b60ab363d0bde8e0
Michael Lill <michael.lill@tokiwa.software>
2023-04-04 13:19:01 +0200

    cleanup: renaming Object->Any, move "unit" to FuzionConstants

--- src/dev/flang/ast/AbstractFeature.java
+++ src/dev/flang/ast/AbstractFeature.java
@@ -732,7 +732,7 @@ public abstract class AbstractFeature extends ANY implements Comparable<Abstract
             var p = pos();
             var typeArg = new Feature(p,
                                       visibility(),
-                                      outer().isUniverse() && featureName().baseName().equals(FuzionConstants.OBJECT_NAME) ? 0 : Consts.MODIFIER_REDEFINE,
+                                      outer().isUniverse() && featureName().baseName().equals(FuzionConstants.ANY_NAME) ? 0 : Consts.MODIFIER_REDEFINE,
                                       selfType(),
                                       FuzionConstants.TYPE_FEATURE_THIS_TYPE,
                                       Contract.EMPTY_CONTRACT,
