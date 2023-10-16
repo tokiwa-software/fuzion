8940b46b9f4a494ee9f0ab0d0cb43e04f426b60e
Max Teufel <max.teufel@tokiwa.software>
2023-10-16 13:20:19 +0200

    partially revert #2009, fix regression in JVM tests

--- src/dev/flang/me/MiddleEnd.java
+++ src/dev/flang/me/MiddleEnd.java
@@ -137,6 +137,8 @@ public class MiddleEnd extends ANY
   void markInternallyUsed() {
     var universe = _mir.universe();
     var m = Clazz._module;
+    markUsed(Types.resolved.f_fuzion_java_object                  , SourcePosition.builtIn);
+    markUsed(Types.resolved.f_fuzion_java_object_ref              , SourcePosition.builtIn);
     markUsed(universe.get(m, "Const_String")                      , SourcePosition.builtIn); // NYI this should be unecessary?
     markUsed(universe.get(m, FuzionConstants.UNIT_NAME)           , SourcePosition.builtIn);
     markUsed(universe.get(m, "void")                              , SourcePosition.builtIn);
