036b0d420be73fe11de47c7ff833b72c5f1798b8
Michael Lill <michael.lill@tokiwa.software>
2023-10-26 11:43:46 +0200

    remove unused imports

--- src/dev/flang/ast/If.java
+++ src/dev/flang/ast/If.java
@@ -27,7 +27,6 @@ Fuzion language implementation.  If not, see <https://www.gnu.org/licenses/>.
 package dev.flang.ast;
 
 import java.util.Iterator;
-import java.util.stream.Stream;
 
 import dev.flang.util.List;
 import dev.flang.util.SourcePosition;
