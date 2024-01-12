8f552656ba74882ee977859a0758d6ba89085651
Michael Lill <michael.lill@tokiwa.software>
2024-01-12 14:23:22 +0100

    fzjava: replace `is` by `=>`c

--- src/dev/flang/tools/fzjava/ForClass.java
+++ src/dev/flang/tools/fzjava/ForClass.java
@@ -473,7 +473,7 @@ class ForClass extends ANY
         data_dynamic.append("\n" +
                             "  # call Java instance method '" + me + "':\n" +
                             "  #\n" +
-                            "  public " + fn + fp + " " + fr + " is\n" +
+                            "  public " + fn + fp + " " + fr + " =>\n" +
                             "    " + ("fuzion.java.call_virtual (" + fr + ") " +
                                       fuzionString(_class.getName()) + " " +
                                       fuzionString(jn) + " " +
@@ -487,7 +487,7 @@ class ForClass extends ANY
         data_static.append("\n" +
                             "  # call Java static method '" + me + "':\n" +
                             "  #\n" +
-                            "  public " + fn + fp + " " + fr + " is\n" +
+                            "  public " + fn + fp + " " + fr + " =>\n" +
                             "    " + ("fuzion.java.call_static (" + fr + ") " +
                                       fuzionString(me.getDeclaringClass().getName()) + " " +
                                       fuzionString(jn) + " " +
@@ -518,7 +518,7 @@ class ForClass extends ANY
     data_static.append("\n" +
                        "  # call Java constructor '" + co + "':\n" +
                        "  #\n" +
-                       "  public " + fn + fp + " " + fr + " is\n" +
+                       "  public " + fn + fp + " " + fr + " =>\n" +
                        "    " + ("fuzion.java.call_constructor (" + fr + ") " +
                                  fuzionString(co.getDeclaringClass().getName()) + " " +
                                  fuzionString(js) + " " +
@@ -550,7 +550,7 @@ class ForClass extends ANY
     data.append("\n" +
                 "  # short-hand to call Java method '" + me + "':\n" +
                 "  #\n" +
-                "  public " + fn0 + fp + " (" + fr + ") is\n" +
+                "  public " + fn0 + fp + " (" + fr + ") =>\n" +
                 "    " + fn + parametersList(pa) + "\n");
   }
 
@@ -577,7 +577,7 @@ class ForClass extends ANY
     data_static.append("\n" +
                        "  # short-hand to call Java constructor '" + co + "':\n" +
                        "  #\n" +
-                       "  public " + fn0 + fp + " (" + fr + ") is\n" +
+                       "  public " + fn0 + fp + " (" + fr + ") =>\n" +
                        "    " + fn + parametersList(pa) + "\n");
   }
 
@@ -996,7 +996,7 @@ class ForClass extends ANY
             data_static.append("\n" +
                                "  # read static Java field '" + fi + "':\n" +
                                "  #\n" +
-                               "  public " + fn + " " + rt + " is\n" +
+                               "  public " + fn + " " + rt + " =>\n" +
                                "    " + ("fuzion.java.get_static_field (" + rt + ") " +
                                          fuzionString(cn) + " " +
                                          fuzionString(jn) + "\n"));
@@ -1006,7 +1006,7 @@ class ForClass extends ANY
             data_dynamic.append("\n" +
                                 "  # read instance Java field '" + fi + "':\n" +
                                 "  #\n" +
-                                "  public " + fn + " " + rt + " is\n" +
+                                "  public " + fn + " " + rt + " =>\n" +
                                 "    " + ("fuzion.java.get_field (" + rt + ") " +
                                           fcn + ".this " +
                                           fuzionString(jn) + "\n"
