f9511bf9bcfb9ea2ae3e0283efaeb791f67cca0a
Michael Lill <michael.lill@tokiwa.software>
2024-01-16 14:06:28 +0100

    be/c: fix comparing `void`-values
    fixes #2434

--- src/dev/flang/be/c/Intrinsics.java
+++ src/dev/flang/be/c/Intrinsics.java
@@ -1276,7 +1276,7 @@ public class Intrinsics extends ANY
 
     CStmnt result;
 
-    if (c._fuir.clazzIsUnitType(rt))
+    if (c._fuir.clazzIsUnitType(rt) || c._fuir.clazzIsVoidType(rt))
       { // unit-type values are always equal:
         result = tmp.assign(new CIdent("true"));
       }
