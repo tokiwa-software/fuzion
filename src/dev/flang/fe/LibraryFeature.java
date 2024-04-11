c539b7a77a2255cb5d1fe90982c26e6ea21dce39
fridis <fridi@fridi.de>
2024-04-11 15:58:39 +0200

    fe: fix merge

--- src/dev/flang/fe/LibraryFeature.java
+++ src/dev/flang/fe/LibraryFeature.java
@@ -687,6 +687,12 @@ public class LibraryFeature extends AbstractFeature
                 { public SourcePosition pos() { return LibraryFeature.this.pos(fpos, fposEnd); } };
               break;
             }
+          case Unit:
+            {
+              x = new AbstractBlock(new List<>())
+                { public SourcePosition pos() { return LibraryFeature.this.pos(fpos, fposEnd); } };
+              break;
+            }
           case InlineArray:
             {
               var codePos   = _libModule.inlineArrayCodePos(iat);
