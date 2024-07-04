9664c66ef05761fc12b3379f7bb2732d8b5c03ba
fridis <fridi@fridi.de>
2024-07-04 15:19:31 +0200

    ast: fix outer in resolution of inherits call during Call.inferGenerics
    `Call.inferGenerics` requires the resolution of the inhertis call, but the outer
    feature used for this was wrong, must be the feature that contains the inherits
    call.

--- src/dev/flang/ast/Call.java
+++ src/dev/flang/ast/Call.java
@@ -2004,7 +2004,7 @@ public class Call extends AbstractCall
               {
                 if (p instanceof Call pc)
                   {
-                    pc.resolveTypes(res, outer);
+                    pc.resolveTypes(res, aft);
                   }
                 var pt = p.type();
                 if (pt != Types.t_ERROR)
