786efe2c2c2097368901554156604c43586d54a0
Michael Lill <michael.lill@tokiwa.software>
2024-07-19 15:11:43 +0200

    fe: fix accessing field of other instance in field init
    this example was broken previous to this PR
    ```
      ring(r ring) is
        last ring := r.last
    ```

--- src/dev/flang/fe/SourceModule.java
+++ src/dev/flang/fe/SourceModule.java
@@ -1212,6 +1212,9 @@ A post-condition of a feature that does not redefine an inherited feature must s
    * @param v
    * @return
    */
+  @SuppressWarnings({
+    "rawtypes", "unchecked"
+  })
   private boolean inScope(Expr use, AbstractFeature v)
   {
     // we only need to do this evaluation for:
@@ -1307,7 +1310,12 @@ A post-condition of a feature that does not redefine an inherited feature must s
 
         if (f.isField())
           {
-            if (useIsBeforeDefinition[0])
+            /**
+              * cases like this are okay:
+              *   ring(r ring) is
+              *      last ring := r.last
+              */
+            if (useIsBeforeDefinition[0] && !(use instanceof AbstractCall ac && ac.target() instanceof AbstractCall))
               {
                 return false;
               }
