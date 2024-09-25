ab7fe23291116b8708bea0928797ad2119b7b1bc
Michael Lill <michael.lill@tokiwa.software>
2024-09-25 09:42:32 +0200

    fix comments: source of ...
    [ci skip]

--- src/dev/flang/ast/This.java
+++ src/dev/flang/ast/This.java
@@ -20,7 +20,7 @@ Fuzion language implementation.  If not, see <https://www.gnu.org/licenses/>.
  *
  * Tokiwa Software GmbH, Germany
  *
- * Source of class Self
+ * Source of class This
  *
  *---------------------------------------------------------------------*/
 
