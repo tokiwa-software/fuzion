3b08a18b5b6da06c4b7077ae897dbe069c813562
Michael Lill <michael.lill@tokiwa.software>
2024-11-06 11:10:17 +0200

    Update src/dev/flang/ast/Call.java
    Co-authored-by: Fridtjof Siebert <48821596+fridis@users.noreply.github.com>

--- src/dev/flang/ast/Call.java
+++ src/dev/flang/ast/Call.java
@@ -1345,8 +1345,9 @@ public class Call extends AbstractCall
                 var frmlTs = g.replaceOpen(g.feature() == _calledFeature
                                            ? _generics
                                            : (heir.isGenericArgument() // see for example #1919
-                                                ? heir.genericArgument().constraint(res, context).generics()
-                                                : heir.generics()));
+                                                ? heir.genericArgument().constraint(res, context)
+                                                : heir
+                                             ).generics());
                 addToResolvedFormalArgumentTypes(res, argnum + i, frmlTs.toArray(new AbstractType[frmlTs.size()]), frml);
                 i   = i   + frmlTs.size() - 1;
                 cnt = cnt + frmlTs.size() - 1;
