2315cd066f4e7d2e3e7598b6a75bdc43b2086c32
fridis <fridi@fridi.de>
2024-11-27 18:05:50 +0100

    fuir: remove duplicate call to replace_this_Type_by_actual_outer2

--- src/dev/flang/fuir/Clazz.java
+++ src/dev/flang/fuir/Clazz.java
@@ -2047,10 +2047,6 @@ class Clazz extends ANY implements Comparable<Clazz>
                                                    Context.NONE);
         oc = oc.getOuter(o);
         o = (LibraryFeature) o.outer();
-        if (oc != null)
-          {
-            t1 = t1.replace_this_type_by_actual_outer(oc._type, Context.NONE);
-          }
       }
 
     var t2 = replaceThisType(t1);
