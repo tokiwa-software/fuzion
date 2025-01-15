1f7022adb3726b35312eed173f8bc35bb7cedbcf
Michael Lill <michael.lill@tokiwa.software>
2025-01-15 11:31:09 +0100

    fe: re-enable partial application for expr in parentheses
    fixes #4503

--- src/dev/flang/ast/ParsedOperatorCall.java
+++ src/dev/flang/ast/ParsedOperatorCall.java
@@ -118,16 +118,6 @@ public class ParsedOperatorCall extends ParsedCall
     _inParentheses = true;
   }
 
-
-  @Override
-  void propagateForPartial(Resolution res, Context context)
-  {
-    if (!_inParentheses)
-      {
-        super.propagateForPartial(res, context);
-      }
-  }
-
 }
 
 /* end of file */
