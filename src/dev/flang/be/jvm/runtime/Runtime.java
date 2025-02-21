3ece38970a5f708311d3f5e0684020ed34ea676b
Michael Lill <michael.lill@tokiwa.software>
2025-02-21 14:49:23 +0100

    wip

--- src/dev/flang/be/jvm/runtime/Runtime.java
+++ src/dev/flang/be/jvm/runtime/Runtime.java
@@ -65,7 +65,6 @@ import java.util.WeakHashMap;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-import dev.flang.be.interpreter.boolValue;
 import dev.flang.util.ANY;
 import dev.flang.util.Errors;
 import dev.flang.util.JavaInterface;
