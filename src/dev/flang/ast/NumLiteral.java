bc6ca16a1427a127a1003c146b699e6d7fc9f0d7
Simon von Hackewitz <simon.von.hackewitz@tokiwa.software>
2025-03-27 10:28:13 +0100

    ast: cleanup: remove nyi comment

--- src/dev/flang/ast/NumLiteral.java
+++ src/dev/flang/ast/NumLiteral.java
@@ -439,8 +439,6 @@ public class NumLiteral extends Constant
   @Override
   AbstractType typeForUnion()
   {
-    // NYI: UNDER DEVELOPMENT: This seems to work even if we always return null
-    // here.  Need to check if we can just always return null for a union.
     return null;
   }
 
