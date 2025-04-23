8eaa5f52bf1a7f46bc147a5768b343a61575f486
Michael Lill <michael.lill@tokiwa.software>
2025-04-23 11:08:50 +0200

    windows/tests: add skip_win option

--- bin/run_tests_parallel.sh
+++ bin/run_tests_parallel.sh
@@ -105,7 +105,10 @@ for test in $TESTS; do
     if test -n "$VERBOSE"; then
       printf '\nrun %s: ' "$test"
     fi
-    if test -e "$test"/skip -o -e "$test"/skip_"$TARGET"; then
+    if [ "${OS-default}" = "Windows_NT" ] && test -e "$test"/skip_win; then
+      printf "_"
+      echo "$test: skipped" >>"$BUILD_DIR"/run_tests.results
+    elif test -e "$test"/skip -o -e "$test"/skip_"$TARGET"; then
       printf "_"
       echo "$test: skipped" >>"$BUILD_DIR"/run_tests.results
     else
