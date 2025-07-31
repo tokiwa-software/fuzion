6e87599dfd2feec0b212a34b3c4f0807afd646a3
Michael Lill <michael.lill@tokiwa.software>
2025-07-31 17:44:27 +0200

    fix require-condition

--- src/dev/flang/ast/Call.java
+++ src/dev/flang/ast/Call.java
@@ -477,7 +477,7 @@ public class Call extends AbstractCall
       : result.selfOrConstraint(res, context);
 
     // NYI: CLEANUP:
-    if (result != null && target().isTypeAsValueCall() && !result.feature().isCotype())
+    if (result != null && result != Types.t_ERROR && target().isTypeAsValueCall() && !result.feature().isCotype())
       {
         result = (res != null
           ? res.cotype(result.feature())
