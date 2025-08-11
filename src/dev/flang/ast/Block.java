ce430ea7d6b82ec1513c234feaf55634b9bccb80
Michael Lill <michael.lill@tokiwa.software>
2025-08-11 09:19:51 +0200

    ast: block simplify propagateExpectedType

--- src/dev/flang/ast/Block.java
+++ src/dev/flang/ast/Block.java
@@ -343,15 +343,14 @@ public class Block extends AbstractBlock
    */
   Expr propagateExpectedType(Resolution res, Context context, AbstractType type, Supplier<String> from)
   {
-    if (type.compareTo(Types.resolved.t_unit) == 0 && hasImplicitResult())
-      { // return unit if this is expected even if we would implicitly return
-        // something else:
-        _expressions.add(new Block(new List<>()));
-      }
-
-    Expr resExpr = resultExpression();
     Expr result = this;
-    if (resExpr != null)
+    Expr resExpr = resultExpression();
+    if (type.compareTo(Types.resolved.t_unit) == 0 && hasImplicitResult() ||
+        resExpr == null && Types.resolved.t_unit.compareTo(type) != 0)
+      {
+        _expressions.add(Types.resolved.unitCall(res, context));
+      }
+    else if (resExpr != null)
       {
         // this may do partial application for the whole block
         result = super.propagateExpectedType(res, context, type, from);
@@ -364,10 +363,6 @@ public class Block extends AbstractBlock
             _expressions.remove(idx);
             _expressions.add(x);
           }
-      }
-    else if (Types.resolved.t_unit.compareTo(type) != 0)
-      {
-        _expressions.add(new Call(pos(), FuzionConstants.UNIT_NAME).resolveTypes(res, context));
       }
     return result;
   }
