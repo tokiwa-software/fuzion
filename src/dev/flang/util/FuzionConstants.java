542edd023141820fa1e5194e0e2c8169c0e9ba76
fridis <fridi@fridi.de>
2025-08-12 15:50:16 +0200

    base: cleanup, renamed `TypedSequence` as `Typed_Sequence`, etc.

--- src/dev/flang/util/FuzionConstants.java
+++ src/dev/flang/util/FuzionConstants.java
@@ -125,7 +125,7 @@ public class FuzionConstants extends ANY
   /**
    * Name of TypedAplicator feature.
    */
-  public static final String TYPED_APPLICATOR_FEAT = "TypedApplicator";
+  public static final String TYPED_APPLICATOR_FEAT = "Typed_Applicator";
 
 
   /**
