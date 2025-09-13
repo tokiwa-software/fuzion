f39a6dd835b9dbf97fdb3c4c7af6fd8c9f4fb54a
fridis <fridi@fridi.de>
2025-09-13 13:21:08 +0200

    ast: fix comment (remove outdated refs to variable that no longer exist)

--- src/dev/flang/fe/SourceModule.java
+++ src/dev/flang/fe/SourceModule.java
@@ -1629,7 +1629,7 @@ A post-condition of a feature that does not redefine an inherited feature must s
                         //
                         //    x(B type, w option B)
                         //
-                        // we must replace `option A` (th) by `option B` (to), i.e.,
+                        // we must replace `option A` by `option B`, i.e.,
                         // replace o's type parameterss by f's:
                         //
                         t -> t.applyTypePars(original, redefinition.generics().asActuals()));
