90b5af227d56dadd2766b82254ba411a997d934e
Michael Lill <michael.lill@tokiwa.software>
2025-10-21 09:39:36 +0200

    ast: remove unused f_error

--- src/dev/flang/ast/Types.java
+++ src/dev/flang/ast/Types.java
@@ -180,8 +180,6 @@ public class Types extends ANY
     public final AbstractFeature f_effect;
     public final AbstractFeature f_effect_finally;
     public final AbstractFeature f_effect_static_finally;
-    public final AbstractFeature f_error;
-    public final AbstractFeature f_error_msg;
     public final AbstractFeature f_fuzion;
     public final AbstractFeature f_fuzion_java;
     public final AbstractFeature f_fuzion_Java_Object;
@@ -255,8 +253,6 @@ public class Types extends ANY
       f_effect                  = universe.get(mod, "effect", 0);
       f_effect_finally          = f_effect.get(mod, "finally", 0);
       f_effect_static_finally   = f_effect.get(mod, "static_finally", 0);
-      f_error                   = universe.get(mod, "error", 1);
-      f_error_msg               = f_error.get(mod, "msg", 0);
       f_fuzion                  = universe.get(mod, "fuzion", 0);
       f_fuzion_java             = f_fuzion.get(mod, "java", 0);
       f_fuzion_Java_Object      = f_fuzion_java.get(mod, "Java_Object", 1);
