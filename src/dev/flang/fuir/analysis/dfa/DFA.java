d13900fa2b2173d63217386dec834d668e09c462
Michael Lill <michael.lill@tokiwa.software>
2025-11-27 14:50:19 +0100

    dfa: refine lteq intrinsic

--- src/dev/flang/fuir/analysis/dfa/DFA.java
+++ src/dev/flang/fuir/analysis/dfa/DFA.java
@@ -1992,10 +1992,10 @@ public class DFA extends ANY
     put("i16.type.equality"              , cl -> numericEquality(cl) );
     put("i32.type.equality"              , cl -> numericEquality(cl) );
     put("i64.type.equality"              , cl -> numericEquality(cl) );
-    put("i8.type.lteq"                   , cl -> cl._dfa.bool() );
-    put("i16.type.lteq"                  , cl -> cl._dfa.bool() );
-    put("i32.type.lteq"                  , cl -> cl._dfa.bool() );
-    put("i64.type.lteq"                  , cl -> cl._dfa.bool() );
+    put("i8.type.lteq"                   , cl -> numericLteq(cl) );
+    put("i16.type.lteq"                  , cl -> numericLteq(cl) );
+    put("i32.type.lteq"                  , cl -> numericLteq(cl) );
+    put("i64.type.lteq"                  , cl -> numericLteq(cl) );
 
     put("u8.prefix -°"                   , cl -> NumericValue.create(cl._dfa, fuir(cl).clazzResultClazz(cl.calledClazz())) );
     put("u16.prefix -°"                  , cl -> NumericValue.create(cl._dfa, fuir(cl).clazzResultClazz(cl.calledClazz())) );
@@ -2046,10 +2046,10 @@ public class DFA extends ANY
     put("u16.type.equality"              , cl -> numericEquality(cl) );
     put("u32.type.equality"              , cl -> numericEquality(cl) );
     put("u64.type.equality"              , cl -> numericEquality(cl) );
-    put("u8.type.lteq"                   , cl -> cl._dfa.bool() );
-    put("u16.type.lteq"                  , cl -> cl._dfa.bool() );
-    put("u32.type.lteq"                  , cl -> cl._dfa.bool() );
-    put("u64.type.lteq"                  , cl -> cl._dfa.bool() );
+    put("u8.type.lteq"                   , cl -> numericLteq(cl) );
+    put("u16.type.lteq"                  , cl -> numericLteq(cl) );
+    put("u32.type.lteq"                  , cl -> numericLteq(cl) );
+    put("u64.type.lteq"                  , cl -> numericLteq(cl) );
 
     put("i8.as_i32"                      , cl -> NumericValue.create(cl._dfa, fuir(cl).clazzResultClazz(cl.calledClazz())) );
     put("i16.as_i32"                     , cl -> NumericValue.create(cl._dfa, fuir(cl).clazzResultClazz(cl.calledClazz())) );
@@ -2379,6 +2379,23 @@ public class DFA extends ANY
     return v0 == null || v1 == null
       ? cl._dfa.bool()
       : cl._dfa.boolAsVal(v0 == v1);
+  }
+
+
+  /**
+   * returns bool, if exact value of one of the args is not known,
+   * otherwise true or false.
+   *
+   * @param cl
+   * @return
+   */
+  private static Value numericLteq(Call cl)
+  {
+    var v0 = (cl._args.get(0).value() instanceof NumericValue nv) ? nv._value : null;
+    var v1 = (cl._args.get(1).value() instanceof NumericValue nv) ? nv._value : null;
+    return v0 == null || v1 == null
+      ? cl._dfa.bool()
+      : cl._dfa.boolAsVal(v0 <= v1);
   }
 
 
