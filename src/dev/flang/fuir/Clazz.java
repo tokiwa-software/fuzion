001eb6239592f644a042c27d809876ce7de17622
Michael Lill <michael.lill@tokiwa.software>
2025-12-02 14:33:38 +0100

    fuir: remove special clazz unit

--- src/dev/flang/fuir/Clazz.java
+++ src/dev/flang/fuir/Clazz.java
@@ -764,14 +764,10 @@ class Clazz extends ANY implements Comparable<Clazz>
       }
 
     var res = YesNo.no;
-    if (_specialClazzId == SpecialClazzes.c_unit)
-      {
-        res = YesNo.yes;
-      }
-    else if ( _fuir._lookupDone && (isValue()                       &&
-                                    !feature().isBuiltInPrimitive() &&
-                                    !isVoidType()                   &&
-                                    !isChoice()                       ))
+    if (isValue()                       &&
+        !feature().isBuiltInPrimitive() &&
+        !isVoidType()                   &&
+        !isChoice())
       {
         // Tricky: To avoid endless recursion, we set _isUnitType to No. In case we
         // have a recursive type, isUnitType() will return false, so recursion will
