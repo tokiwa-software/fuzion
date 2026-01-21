efe256529cf9e602eca8c3dc7cf14ac9a0f3194b
Michael Lill <michael.lill@tokiwa.software>
2026-01-21 10:20:50 +0100

    fuir: remove hack for compile_time_type_casts

--- src/dev/flang/fuir/Clazz.java
+++ src/dev/flang/fuir/Clazz.java
@@ -1032,9 +1032,6 @@ class Clazz extends ANY implements Comparable<Clazz>
   {
     return isVoidType()
       ? this
-      // NYI: HACK for test compile_time_type_casts
-      : !feature().inheritsFrom(c.calledFeature().outer())
-      ? this
       : lookup(new FeatureAndActuals(c.calledFeature(),
                                      typePars),
                c.select(),
