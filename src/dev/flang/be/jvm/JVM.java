109f61363308d5262f62bdda5a4aca5f505a4c3d
Michael Lill <michael.lill@tokiwa.software>
2026-01-21 09:31:29 +0100

    java, use try-with-resource where possible
    (done by AI)

--- src/dev/flang/be/jvm/JVM.java
+++ src/dev/flang/be/jvm/JVM.java
@@ -865,21 +865,22 @@ should be avoided as much as possible.
       {
         _options.verbosePrintln(" + " + executableName);
         var f = executableName.toFile();
-        var out = new PrintWriter(new FileOutputStream(f));
-        out.println(String.format(// NYI: UNDER DEVELOPMENT: This probably needs to be changed for Windows:
-                                  """
-                                  #!/bin/sh
+        try (var out = new PrintWriter(new FileOutputStream(f)))
+          {
+            out.println(String.format(// NYI: UNDER DEVELOPMENT: This probably needs to be changed for Windows:
+                                      """
+                                      #!/bin/sh
 
-                                  SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+                                      SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 
-                                  LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_PATH" \
-                                  PATH="$PATH:$SCRIPT_PATH" \
-                                  DYLD_FALLBACK_LIBRARY_PATH="$DYLD_FALLBACK_LIBRARY_PATH:$SCRIPT_PATH" \
-                                  java --enable-preview --enable-native-access=ALL-UNNAMED -D%s="$0" %s "$@"
-                                  """,
-                                  FUZION_COMMAND_PROPERTY,
-                                  args));
-        out.close();
+                                      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_PATH" \
+                                      PATH="$PATH:$SCRIPT_PATH" \
+                                      DYLD_FALLBACK_LIBRARY_PATH="$DYLD_FALLBACK_LIBRARY_PATH:$SCRIPT_PATH" \
+                                      java --enable-preview --enable-native-access=ALL-UNNAMED -D%s="$0" %s "$@"
+                                      """,
+                                      FUZION_COMMAND_PROPERTY,
+                                      args));
+          }
         f.setExecutable(true);
         for (String str : new List<>("libfuzion_rt.so", "libfuzion_rt.dylib", "fuzion_rt.dll"))
           {
