e6799e5b89747399a41d9c74d10f459c4a9eea00
Michael Lill <michael.lill@tokiwa.software>
2026-02-16 12:21:41 +0100

    Revert "be/c: use -fstack-protector-strong, -fstack-clash-protection, -fcf-pr…"

--- src/dev/flang/be/c/C.java
+++ src/dev/flang/be/c/C.java
@@ -847,9 +847,6 @@ public class C extends ANY
     // https://lobste.rs/s/avrfxz/ubuntu_24_04_lts_will_enable_frame
     command.addAll("-fno-omit-frame-pointer", "-mno-omit-leaf-frame-pointer");
 
-    // select due to: dpkg-buildflags --get CFLAGS
-    command.addAll("-fstack-protector-strong", "-fstack-clash-protection", "-fcf-protection");
-
     command.add("-lm");
 
     if (usesThreads() && !isWindows())
