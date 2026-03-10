dce534d790d645b4eab45f1ae653420baa40a9b0
Michael Lill <michael.lill@tokiwa.software>
2026-03-10 11:34:12 +0100

    ast: attempt to fix #1207, wrong application of type pars
    fixes #1207

--- src/dev/flang/ast/AbstractType.java
+++ src/dev/flang/ast/AbstractType.java
@@ -1166,7 +1166,7 @@ public abstract class AbstractType extends ANY implements Comparable<AbstractTyp
     var result = this;
     if (dependsOnGenerics())
       {
-        result = result.applyTypeParsLocally(target.feature(), target.generics(), select);
+        result = result.applyTypeParsLocally(target.feature(), target.actualGenerics(), select);
       }
     return result;
   }
