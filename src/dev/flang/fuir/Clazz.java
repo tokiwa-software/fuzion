733c1e5d617bca11241cae896b096f659d4b42b7
Michael Lill <michael.lill@tokiwa.software>
2026-04-02 11:31:59 +0200

    apply review comments

--- src/dev/flang/fuir/Clazz.java
+++ src/dev/flang/fuir/Clazz.java
@@ -553,13 +553,13 @@ class Clazz extends ANY implements Comparable<Clazz>
       {
         result = new TreeSet<Clazz>();
         result.add(this);
-        var inh = feature().inherits();
-        for (var parent : inh)
+        for (var inh_call : feature().inherits())
           {
-            var pt = parent.type();
+            var pt = inh_call.type();
             var t1 = feature().handDownAndApply(pt, _type);
-            var t2 = handDown(t1, NO_SELECT, (_,_)->{}, inh);
-            var t3 = replaceThisType(t2, inh);
+            // NYI: CLEANUP: @fridi "I am not sure if or why handDown and replaceThisType are needed here at all."
+            var t2 = handDown(t1, NO_SELECT, (_,_)->{}, new List<>());
+            var t3 = replaceThisType(t2, new List<>());
             var pc  = _fuir.newClazz(t3);
 
             if (!result.contains(pc))
