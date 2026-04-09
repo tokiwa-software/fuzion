577ae74b0ed61bfc61c044db776849084f98feed
Max Teufel <max.teufel@tokiwa.software>
2026-04-09 11:45:54 +0200

    include/posix.c: add static assert for magic numbers

--- include/posix.c
+++ include/posix.c
@@ -74,6 +74,11 @@ static_assert(SIGPIPE == 13, "signal definition different than expected");
 static_assert(SIGALRM == 14, "signal definition different than expected");
 static_assert(SIGTERM == 15, "signal definition different than expected");
 
+static_assert(SCHED_OTHER == 0, "magic number different than expected");
+static_assert(SCHED_FIFO == 1, "magic number different than expected");
+static_assert(SCHED_RR == 2, "magic number different than expected");
+static_assert(ENOSYS == 38, "error number different than expected");
+
 
 // thread local to hold the last
 // error that occurred in fuzion runtime.
