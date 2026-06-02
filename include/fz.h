17ea178274eda31c4120a1fd8d6f250caf138dc1
Max Teufel <max.teufel@tokiwa.software>
2026-06-02 13:11:17 +0200

    os: add basic support for creating pipes

--- include/fz.h
+++ include/fz.h
@@ -474,6 +474,16 @@ int fzE_process_create(char * args[], size_t argsLen, char * env[], size_t envLe
 int64_t fzE_process_poll(int64_t p);
 
 /**
+ * open a new pipe, put read and write end into array passed here
+ *
+ * @param fds array to be filled with pipe's file descriptors
+ *
+ * @return non-zero error number, or 0 on success
+ *
+ */
+int fzE_pipe_create(int64_t * fds);
+
+/**
  * read nbytes bytes into `buf` from pipe `desc`.
  *
  * @return -1 error, bytes read on success
