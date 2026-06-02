1f1471d8503c7089566838ef29d4eab80aff6dbb
Michael Lill <michael.lill@tokiwa.software>
2026-06-02 13:10:20 +0200

    parser: use sourceRange for position of an `if`

--- src/dev/flang/parser/Parser.java
+++ src/dev/flang/parser/Parser.java
@@ -2965,7 +2965,7 @@ ifexpr      : "if" exprInLine thenPart elseBlockOpt
 
         if (oldMinIdent != null) { setMinIndent(oldMinIdent); }
 
-        return Match.createIf(pos, e, b,
+        return Match.createIf(sourceRange(pos), e, b,
           // do no use empty blocks as else blocks since the source position
           // of those block might be somewhere unexpected.
           els != null && els._expressions.size() > 0 ? els : null,
