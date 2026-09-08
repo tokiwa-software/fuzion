d47adbba563d4defbaba3140c77591f7b984951d
Michael Lill <michael.lill@tokiwa.software>
2026-09-08 12:15:19 +0200

    be/int: fix boxing

--- src/dev/flang/be/jvm/CodeGen.java
+++ src/dev/flang/be/jvm/CodeGen.java
@@ -960,6 +960,7 @@ class CodeGen
     var res = Expr
       .comment("box from " + clazzInQuotes(vc) + " to " + clazzInQuotes(rc))
       .andThen(val)
+      // NYI: BUG: .andThen(_jvm.cloneValue(s, val, vc, NO_CLAZZ))
       .andThen(Expr.invokeStatic(n, Names.BOX_METHOD_NAME,
                                   _types.boxSignature(rc),
                                   _types.javaType(rc)));
