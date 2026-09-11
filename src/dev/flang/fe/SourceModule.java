6a37d4724844d5ef77b743ae46b30b62c4074d0d
Artur Dmitriienko <diedivedrive@gmail.com>
2026-09-11 10:53:07 +0200

    fe: added the forgotten parenthesis

--- src/dev/flang/fe/SourceModule.java
+++ src/dev/flang/fe/SourceModule.java
@@ -957,8 +957,8 @@ A feature that does not redefine an inherited feature must not use the `redef` m
             List<FeatureAndOuter> hiddenFeaturesSameSignature = lookup(outer, f.baseName(), null, true, true)
               .stream()
               .filter(fo -> fo._feature != f // excluding the feature itself
-                          && !visibleFor(fo._feature, f)) //only truly invisible features
-                          && fo._feature.featureName().equals(f.featureName())
+                          && !visibleFor(fo._feature, f) //only truly invisible features
+                          && fo._feature.featureName().equals(f.featureName()))
               .collect(List.collector());
             AstErrors.redefineModifierDoesNotRedefine(f, hiddenFeaturesSameSignature);
           }
