3d8ad15b847d0e05b2b9935808127448c57e81b7
Michael Lill <michael.lill@tokiwa.software>
2026-09-23 16:02:15 +0200

    ast: Call, refine targetErroneous

--- src/dev/flang/ast/Call.java
+++ src/dev/flang/ast/Call.java
@@ -415,9 +415,11 @@ public class Call extends AbstractCall
    * Helper to check if the target of this call is erroneous, i.e., it might
    * have a pending error.
    */
-  private boolean targetErroneous()
+  private boolean targetErroneous(Resolution res, Context context)
   {
-    return _target != null && _target.asParsedType() == null && _target.type() == Types.t_ERROR;
+    return _target != null &&
+      (_target.asParsedType() == null || (_target.asParsedType() != null && _target.asParsedType().resolve(res, context, true) == null)) &&
+      _target.type() == Types.t_ERROR;
   }
 
 
@@ -651,7 +653,7 @@ public class Call extends AbstractCall
     if (POSTCONDITIONS) ensure
       (Errors.any() || !calledFeatureKnown() || _calledFeature != Types.f_ERROR || targetVoid,
        Errors.any() || _target        != Call.ERROR,
-       Errors.any() || _calledFeature != null || _pendingError != null || targetErroneous(),
+       Errors.any() || _calledFeature != null || _pendingError != null || targetErroneous(res, context),
        Errors.any() || _target        != null || _pendingError != null);
 
     return !targetVoid;
@@ -2686,7 +2688,7 @@ public class Call extends AbstractCall
     // Check that we either know _calledFeature, or there is an error pending
     // either for this Call, or we have a problem with the target:
     if (PRECONDITIONS) require
-      (Errors.any() || res._options.isLanguageServer() || _calledFeature != null || _pendingError != null || targetErroneous());
+      (Errors.any() || res._options.isLanguageServer() || _calledFeature != null || _pendingError != null || targetErroneous(res, context));
 
     if (_calledFeature == Types.f_ERROR)
       {
@@ -2717,7 +2719,7 @@ public class Call extends AbstractCall
         resolveTypesOfActuals(res, context);
         notifyInferred();
 
-        result = isErroneous(res)
+        result = isErroneous(res, context)
           ? resolveTypesErrorResult()
           : resolveTypesSuccessResult(res, context);
       }
@@ -2781,7 +2783,7 @@ public class Call extends AbstractCall
       }
 
     if (POSTCONDITIONS) ensure
-      (targetErroneous() || _pendingError != null || Errors.any() || result.typeForInferencing() != Types.t_ERROR || result == Call.ERROR);
+      (targetErroneous(res, context) || _pendingError != null || Errors.any() || result.typeForInferencing() != Types.t_ERROR || result == Call.ERROR);
 
     return  result;
   }
@@ -2790,10 +2792,10 @@ public class Call extends AbstractCall
   /**
    * Is this call in an erroneous state?
    */
-  private boolean isErroneous(Resolution res)
+  private boolean isErroneous(Resolution res, Context context)
   {
     return !res._options.isLanguageServer() &&
-      (targetErroneous() || _pendingError == null && typeForInferencing() == Types.t_ERROR);
+      (targetErroneous(res, context) || _pendingError == null && typeForInferencing() == Types.t_ERROR);
   }
 
 
