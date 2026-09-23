6b1dd103ef55fc0f7c116d9c088090833357dd0a
Michael Lill <michael.lill@tokiwa.software>
2026-09-23 14:34:52 +0200

    win: assert non zero truncation
    AI claimed this might be a problem?

--- include/win.c
+++ include/win.c
@@ -462,6 +462,8 @@ uint64_t fzE_posix_time(int clockid)
       exit(EXIT_FAILURE);
   }
 
+  assert( 1000000000ULL / frequency.QuadPart != 0ULL );
+
   return (uint64_t)(counter.QuadPart * (1000000000ULL / frequency.QuadPart));
 }
 
